05a011ed0951fe73070276fcecd5c07dc5ac60cd
AndreColombo <rm15019@fiap.com.br>
2023-11-07 13:44:44 -0300

    André - AC DE INW E LP2

--- saudebem.js
+++ saudebem.js
@@ -11,8 +11,6 @@ const port = 3000;
 
 // Acesso ao mongodb
 mongoose.connect("mongodb://127.0.0.1:27017/saudebem", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverSelectionTimeoutMS: 10000,
 });
 
@@ -24,43 +22,107 @@ const UsuarioSchema = new mongoose.Schema({
 
 const Usuario = mongoose.model("Usuario", UsuarioSchema);
 
-// Configurando os roteamentos Cadastro Usuário
+// Rota de Cadastro de Usuário
 app.post("/cadastrousuario", async (req, res) => {
   const email = req.body.email;
   const senha = req.body.senha;
+
+  // testando se todos os campos foram preenchidos
+  if (email == null || senha == null) {
+    return res.status(400).json({ error: "Preencha todos os campos" });
+  }
+
+  // teste mais importante da AC
+  const emailExiste = await Usuario.findOne({ email: email });
+
+  if (emailExiste) {
+    return res.status(400).json({ error: "O email cadastrado já existe" });
+  }
 
   const usuario = new Usuario({
     email: email,
     senha: senha,
   });
+
+  try {
+    const newUsuario = await usuario.save();
+    res.json({
+      error: null,
+      msg: "Cadastro de usuário realizado com sucesso",
+      usuarioId: newUsuario._id,
+    });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 // Model Produtoentrega
 const ProdutoentregaSchema = new mongoose.Schema({
-  id_produtoentrega: { type: Number, required: true },
+  idprodutoentrega: { type: Number, required: true },
   descricao: { type: String },
   laboratorio: { type: String },
-  data_validade: { type: Date },
-  quantidade_estoque: { type: Number },
+  datavalidade: { type: Date },
+  quantidadeestoque: { type: Number },
 });
 
 const Produtoentrega = mongoose.model("Produtoentrega", ProdutoentregaSchema);
 
-// Configurando os roteamentos Cadastro Produtoentrega
+// Rota de Cadastro de Produtoentrega
 app.post("/cadastroprodutoentrega", async (req, res) => {
-  const id_produtoentrega = req.body.id_produtoentrega;
+  const idprodutoentrega = req.body.idprodutoentrega;
   const descricao = req.body.descricao;
   const laboratorio = req.body.laboratorio;
-  const data_validade = req.body.data_validade;
-  const quantidade_estoque = req.body.quantidade_estoque;
+  const datavalidade = req.body.datavalidade;
+  const quantidadeestoque = req.body.quantidadeestoque;
 
-  const produto = new Produto({
-    id_produtoentrega: id_produtoentrega,
+  // testando se todos os campos foram preenchidos
+  if (
+    idprodutoentrega == null ||
+    descricao == null ||
+    laboratorio == null ||
+    datavalidade == null ||
+    quantidadeestoque == null
+  ) {
+    return res.status(400).json({ error: "Preencha todos os campos" });
+  }
+
+  if (quantidadeestoque > 11) {
+    return res
+      .status(400)
+      .json({ error: "Limite de estoque superado, impossível cadastrar" });
+  } else if (quantidadeestoque <= 0) {
+    return res
+      .status(400)
+      .json({ error: "Digite um valor positivo menor ou igual a 11" });
+  }
+
+  // teste mais importante da AC
+  const idExiste = await Produtoentrega.findOne({
+    idprodutoentrega: idprodutoentrega,
+  });
+
+  if (idExiste) {
+    return res.status(400).json({ error: "O id cadastrado já existe" });
+  }
+
+  const produto = new Produtoentrega({
+    idprodutoentrega: idprodutoentrega,
     descricao: descricao,
     laboratorio: laboratorio,
-    data_validade: data_validade,
-    quantidade_estoque: quantidade_estoque,
+    datavalidade: datavalidade,
+    quantidadeestoque: quantidadeestoque,
   });
+
+  try {
+    const newProduto = await produto.save();
+    res.json({
+      error: null,
+      msg: "Cadastro de produto entregue realizado com sucesso",
+      produtoId: newProduto._id,
+    });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 // Rota Padrão
